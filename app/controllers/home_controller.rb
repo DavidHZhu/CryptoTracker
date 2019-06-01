110b88e2c65739042b4592ea83308926138d7223
DavidHZhu <dhzhu@edu.uwaterloo.ca>
2019-06-01 23:48:40 +0000

    Lookup form added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,37 @@
 class HomeController < ApplicationController
+  # List of controllers and methods -> controlled by routes
   def index
+    #require 'net/http'
+    #require 'json'
+    
+    # Fetches CoinMarketCap prices from link
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ["BTC", "STEEM", "ETH", "LTC",]
+    
   end
   
   def about
   end
+  
+  def lookup
+    @symbol = params[:sym]
+    if @symbol
+      @symbol = @symbol.upcase
+    end 
+    
+    if @symbol == ""
+      @symbol = "Please enter a valid currency."
+    end 
+    
+    # Fetches CoinMarketCap prices from link
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup = JSON.parse(@response)
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/about'
+  get 'home/lookup'
+  post 'home/lookup' => 'home/lookup'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
